5f152eeab059c08411b59dff59e4b84934946e3b
ArzimanOff <arzimanovgyulahmed@yandex.ru>
2024-11-27 01:22:47 +0300

    реализовал таймер с помощью сервисов и корутин

--- app/src/main/java/com/arziman_off/learnservices/MainActivity.kt
+++ app/src/main/java/com/arziman_off/learnservices/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
