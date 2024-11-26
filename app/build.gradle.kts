e1cb32b1db892f05140471bf8c6e5ad39d5834a9
ArzimanOff <arzimanovgyulahmed@yandex.ru>
2024-11-27 00:52:09 +0300

    fix (изменил compileSdk)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.arziman_off.learnservices"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.arziman_off.learnservices"
